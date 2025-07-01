2ab8bde4bf5074d78f5a0762849d92598a6a4070
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-07-01 12:55:56 -0300

    Iniciando repositorio API Livros

---
+++ models/Livro.cs
@@ -0,0 +1,26 @@
+namespace CatalogoApi.Models
+{
+    public class Livro
+    {
+        public int Id { get; set; }
+        public string? Titulo { get; set; }
+        public string? Autor { get; set; }
+        public int AnoDePublicacao { get; set; }
+    }
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
