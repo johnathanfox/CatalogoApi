02a19a372945d09b8fb30030134dcb64b2693e92
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-07-01 13:09:39 -0300

    Estruturando Controller

---
+++ Controller/LivrosController.cs
@@ -0,0 +1,98 @@
+using CatalogoApi.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogoApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")] // Rota base: /api/livros
+    public class LivrosController : ControllerBase
+    {
+        // Banco de dados em memória (static para persistir entre requisições)
+        private static List<Livro> _livros = new List<Livro>
+        {
+          new Livro { Id = 1, Titulo = "A arte da guerra", Autor = "Sun Tzu", AnoDePublicacao = -500 },
+          new Livro { Id = 2, Titulo = "O senhor dos Anéis", Autor = "Sun Tzu", AnoDePublicacao = -500 },
+          new Livro { Id = 3, Titulo = "A arte da guerra", Autor = "Sun Tzu", AnoDePublicacao = -500 },
+        };
+
+
+        // --- Inícios das operações do CRUD ---
+
+        // 1. READ: Buscar todos Livros 
+        // GET /api/livros 
+
+        [HttpGet]
+        public IActionResult GetTodos()
+        {
+            return Ok(_livros);
+        }
+
+        // 2. READ: Buscar apenas um livro por ID 
+        // GET /api/livros/{id}
+        [HttpGet("{id}")]
+        public IActionResult GetPorId(int id)
+        { 
+          var livro = _livros  
+        }
+
+
+
+
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
