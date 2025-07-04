62dcf82f49fdcd49a757675acd47a51ff43d61de
Johnathan Martins <johnathanfoxandrade@gmail.com>
2025-07-04 03:29:27 -0300

    att

--- obj/Debug/net9.0/CatalogoApi.AssemblyInfo.cs
+++ obj/Debug/net9.0/CatalogoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+efc9f0960d39b3168cad19d0c41d8a79bb233db7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34ec85a37e56e81056b203f3b3cd78005c99c7f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CatalogoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
